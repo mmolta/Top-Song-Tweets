09bcf4877718a393d50294356cfd5267bb9e8670
Marc <mcmolta10@gmail.com>
2017-07-17 22:38:35 -0400

    twitter app initialized

---
+++ index.js
@@ -0,0 +1,9 @@
+const twit = require('twit')
+const request = require('request')
+
+let codes = new twit({
+	consumer_key: YmfMdmMVJotxKyJl85DBSRLZm,
+	consumer_secret: LFSWBoVT8n8QiCtOCt96cIrhO2FLdTYsPDJQqUg4H10y3qcYPA,
+	access_token: 	1694764148-LnNfJyMcHPIOTGh2OMhBd3z5jGx9tHpOJbuT1MO,
+	access_token_secret: Rs7LFhreS7kF17I5cI66m2qP7rkvbPpFpyNvJDquhrdEI
+})
